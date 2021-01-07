50fd0a247e0fa474d13a3ae3cb257d739427dd3d
LauraJimenezB <laura271260@gmail.com>
2021-01-07 14:42:55 -0500

    cocina.js

--- my-app/src/cocina.js
+++ my-app/src/cocina.js
@@ -1,3 +1,25 @@
+import { useState, useEffect } from 'react';
+import firebase from './firebase';
+
 export function Cocina () {
-    return(<h1>Hola</h1>)
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        firebase.firestore().collection('orders').onSnapshot((snapshot)=>{
+          const orders = snapshot.docs.map((doc)=> ({
+            id: doc.id,
+            ...doc.data()
+          }))
+          setOrders(orders);
+        })
+        }, [])
+
+    const listOrders = orders.map((order)=> 
+    <li key={order.id}>
+        <span>{order.cliente}</span>
+        <div>{order.list}</div>
+        <span>{order.total}</span>
+    </li>
+    )
+    return(<ul>{listOrders}</ul>)
 }
