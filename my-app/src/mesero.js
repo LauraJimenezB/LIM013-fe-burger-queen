e7e5fadabdffefcaf03bc7bd59b1021d98cb7e3f
estefaniamoncada <estefaniamr716@gmail.com>
2021-01-14 09:23:22 -0600

    boton de pedidos listos y boton back

--- my-app/src/mesero.js
+++ my-app/src/mesero.js
@@ -1,6 +1,8 @@
 import { VistaMenu } from './vistaMenu.js';
 import burguer from './burguer.svg';
 import './Pedidos.css';
+import { PedidosListos } from './pedidosListos.js';
+import { Back } from './back.js';
 
 export function Mesero() {
  
@@ -8,7 +10,8 @@ export function Mesero() {
       <div>
         <header>
           <div className='logoImg'><img src={burguer} className="Logo" alt="logo"/></div>
-          <h1>BURGER QUEEN</h1>
+          <h1>BURGER QUEEN <PedidosListos/> <Back/></h1>
+          
         </header>
         <VistaMenu/>
       </div>
---
+++ my-app/src/vistaPedidosListos.js
@@ -0,0 +1,11 @@
+import burguer from './burguer.svg';
+
+export function Hola() {
+     return(<div>
+      <header>
+      <div className='logoImg'><img src={burguer} className="Logo" alt="logo"/></div>
+      <h1>BURGER QUEEN</h1>
+    </header>
+    </div>
+    )
+}
